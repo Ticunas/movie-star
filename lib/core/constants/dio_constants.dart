0d3b3c3aa01d261be0d9c5e6a540b0374027116c
Tadman Reis <treis@magnasistemas.com.br>
2021-10-18 16:00:48 -0300

    feat: add base url for posters

--- lib/core/constants/dio_constants.dart
+++ lib/core/constants/dio_constants.dart
@@ -3,3 +3,5 @@ const int receiveTimeout = 5000;
 
 const String baseMovieDbUrl = 'https://api.themoviedb.org/3';
 const String baseGravatarUrl = 'https://gravatar.com/avatar';
+const String basePosterUrl =
+    'https://www.themoviedb.org/t/p/w220_and_h330_face';
